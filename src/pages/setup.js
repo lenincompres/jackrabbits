81661f01572c2f2e27dfdbfe9e09586d82ae35db
Lenin Compres <lenincompres@gmail.com>
2025-03-16 21:28:29 -0400

    update

--- src/pages/setup.js
+++ src/pages/setup.js
@@ -23,7 +23,7 @@ const setupPage = {
       ASSETS.thumbnailOf("sample", Copy.lang === Copy.LANG.es.code ? '9.5em' : '8em', e => popUp("sample")),
       Copy.text({
         es: `El reino de las cartas y sus templos
-          se ensambla como un puzle aleatorio;
+          se ensambla en un proceso aleatorio;
           comienza por armar el del <a class="pop" onclick="popUp('sample')"><b>ejemplo</b></a>
           o anímate a <a class="pop" onclick="popUp('board')"><b>crear el territorio</b></a>.`,
         en: `The lands you connect and spread
