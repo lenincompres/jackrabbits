e05989b67fdf50b2cf5edded5c61a2aab60738e3
Lenin Compres <lenincompres@gmail.com>
2025-04-30 10:46:38 -0400

    update

--- src/pages/sections/strategies.js
+++ src/pages/sections/strategies.js
@@ -186,7 +186,7 @@ const strategySections = [{
     en: `If agreed when the game starts,
     players may trade in cahoot:
     meet to buy with diamond cards 
-    while taking turns on their route.
+    on their turn along their route.
     Keep concealed your hand’s rewards—
     strike a deal for your pursuit.
     If cards traded prove a farce,
