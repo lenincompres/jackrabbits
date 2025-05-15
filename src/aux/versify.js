6c8e84fbdf781205e5460f8074cf1d3e30344459
Lenin Compres <lenincompres@gmail.com>
2025-05-15 13:15:12 -0400

    update

--- src/aux/versify.js
+++ src/aux/versify.js
@@ -1,9 +1,20 @@
 function versify(...args) {
   let poem = [];
   args.forEach(arg => Array.isArray(arg) ? poem.push(...arg) : poem.push(arg));
-  return poem.map(p => typeof p !== "string" ? p : ({
-    span: p.split('\n').map(line => line.trim() + (line.endsWith("—") || line.endsWith('--</span>') ? "" : "&nbsp;")),
-  }), "content");
+  let stanzas = poem.map(p => {
+    if (typeof p !== "string") return p;
+    let spans = p.split('\n').map((line, i) => line.trim() + (line.endsWith("—") || line.endsWith('--</span>') ? "" : "&nbsp;"));
+    let divs = [];
+    for (let i = 0; i < spans.length; i += 2) {
+      divs.push({
+        span: [spans[i], spans[i + 1]],
+      });
+    }
+    return ({
+      div: divs,
+    });
+  });
+  return stanzas;
 };
 
 export default versify;
