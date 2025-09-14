d474d7d369dd27ecb1f32f0328e97cdf56c4b2e0
Lenin Compres <lenincompres@gmail.com>
2025-09-14 09:00:01 -0400

    Update strategies.js

--- src/pages/sections/strategies.js
+++ src/pages/sections/strategies.js
@@ -87,9 +87,9 @@ const strategySections = [{
     en: [
       `You may steal a card at random,
       kicking someone off the board. 
-      Be with them to seize their item— 
-      once you put them to the sword.
-      You go back and forth in tandem,
+      You must meet to seize their item, 
+      And attack them with a sword.
+      Both go back and forth in tandem,
       adding spades to beat what’s scored.
       When no cards are thrown to sum them, 
       then the struggler's kicked off-board.`,
