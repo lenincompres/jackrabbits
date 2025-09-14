4b6c82edb3f6bee978bf5f54871f462aac131fc3
Lenin Compres <lenincompres@gmail.com>
2025-09-14 06:17:44 -0400

    Update transfers.js

--- src/pages/sections/transfers.js
+++ src/pages/sections/transfers.js
@@ -64,7 +64,7 @@ const transferSections = [{
       dashing over tiles in stride.
       Or select a road to port,
       gliding on the seas outside.
-      Choose and ride to your next post, 
+      Choose a ride and reach your post, 
       once you <a class="pop" onclick="popUp('price')">pay what has been priced</a>.
       Landlocked merchants have no boats, 
       since they’re not by the seaside.`,
