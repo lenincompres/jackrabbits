ac9cba6aa787b9683d8e59d34c5595fc5e95e1d6
Lenin Compres <lenincompres@gmail.com>
2025-09-15 09:02:59 -0400

    Update turns.js

--- src/pages/sections/turns.js
+++ src/pages/sections/turns.js
@@ -25,8 +25,8 @@ const turnSections = [{
       `Start your turns by rolling dice,
       so that two results unfold.
       You may draw a one-card prize,
-      if you show a card you hold.
-      Prove it matches value-wise, 
+      if you show a card you hold—
+      prove it matches value-wise, 
       either number you just rolled.`,
       `Please discard if caught with ten.
       Bring your hand to nine again.*`,
