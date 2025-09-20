b810cadcdefa3361f8fd49e3fd4924b79a575353
Lenin Compres <lenincompres@gmail.com>
2025-09-20 08:07:51 -0400

    Update home.js

--- src/pages/home.js
+++ src/pages/home.js
@@ -18,7 +18,7 @@ const homePage = {
         es: `Dirígete al <a href="#setup"><b><big>Inicio</big></b></a> de partida. 
 De un dúo a un cuarteto se convida.`,
         en: `Let's begin the <a href="#setup"><b><big>Setup</big></b></a> phase. 
-Up to four contesters race.`,
+Up to four contenders race.`,
       })),
     },
   },
