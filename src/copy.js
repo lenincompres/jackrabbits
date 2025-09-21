7065ec494e1e72bb5d4c3c4c075e284af015042e
Lenin Compres <lenincompres@gmail.com>
2025-09-21 13:04:04 -0400

    Update copy.js

--- src/copy.js
+++ src/copy.js
@@ -10,7 +10,7 @@ Copy.add({
     en: "Welcome",
   },
   setup: {
-    es: "Inicio</br><small>de partida</small>",
+    es: `Inicio</br><small style="whiteSpace:nowrap">de partida</small>`,
     en: "Setup",
   },
   intro: {
