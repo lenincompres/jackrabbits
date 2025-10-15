31d9d2d8c1048327144f8d808356b1b026f5504c
Lenin Compres <lenincompres@gmail.com>
2025-10-15 10:26:34 -0400

    update

--- src/pages/sections/strategies.js
+++ src/pages/sections/strategies.js
@@ -151,7 +151,7 @@ const strategySections = [{
       Even standing far apart:
       let the proof they're blest be known.
       If you prove at least six hearts, 
-      they'll discard what hearts they've shown.
+      they'll discard the hearts they've shown.
       Either one who's wrong departs—
       off the board the meddler's thrown.`,
     ]
