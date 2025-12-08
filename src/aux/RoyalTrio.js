b16cb1abbaa65105ead2a23e128cc884eed03644
Lenin Compres <lenincompres@gmail.com>
2025-12-08 10:54:03 -0500

    update

--- src/aux/RoyalTrio.js
+++ src/aux/RoyalTrio.js
@@ -15,24 +15,24 @@ export class RoyalTrio extends HTMLElement {
 
     this.set({
       width: "5rem",
-      margin: "0.5rem auto",
+      margin: "1rem auto 0.5rem",
       tag: "figure",
       position: "relative",
       img: {
-        position: "relative",
         zIndex: 1,
+        position: "relative",
         content: ASSETS.rabbitPawn
       },
       i: {
+        lineHeight: "1em",
         textAlign: "right",
         position: "absolute",
-        top: "30%",
-        right: "100%",
-        marginRight: "-1.3rem",
-        whiteSpace: "nowrap",
+        top: "1.5rem",
+        right: this._state.as(s => `${[60, 100][s%2]}%`),
+        marginRight: "-1.2rem",
         transition: "all 0.5s ease",
         opacity: this._state.as(s => 0.8 * (s % 2)),
-        text: this._state.as(s => {
+        content: this._state.as(s => {
           if (s < 3) return Copy.text({
             es: "Sencillo",
             en: "Simple",
@@ -46,8 +46,8 @@ export class RoyalTrio extends HTMLElement {
             en: "Monarchic",
           });
           if (s < 9) return Copy.text({
-            es: "Monárquico & diverso",
-            en: "Diverse-Monarchic",
+            es: "Monárquico</br>& diverso",
+            en: "Diverse-</br>Monarchic",
           });
           return "Tribal";
         }),
@@ -55,12 +55,12 @@ export class RoyalTrio extends HTMLElement {
       div: (new Array(3)).fill(0).map((_, i) => ({
         zIndex: 0,
         position: "absolute",
-        top: "3rem",
-        left: "calc(50% - 1rem)",
-        transform: `rotate(${40 + i * 25}deg)`,
+        top: "2rem",
+        left: "50%",
+        transform: `rotate(${30 + i * 30}deg)`,
         p: {
           textAlign: "center",
-          fontSize: "1.3rem",
+          fontSize: "1.4rem",
           lineHeight: "0.7em",
           padding: "0.2em 0.3em",
           marginLeft: "-0.8em",
@@ -71,7 +71,7 @@ export class RoyalTrio extends HTMLElement {
           backgroundColor: "white",
           boxShadow: "1px 1px 3px black",
           position: "absolute",
-          bottom: this._state.as(s => `${s%2 * 2.5 - 0.2 * i}em`),
+          bottom: this._state.as(s => `${s%2 * 1.6 - 0.2 * i}em`),
           opacity: this._state.as(s => s % 2),
           color: this._state.as(s => {
             if ([3, 4, 7, 8].includes(s)) return this.colors.shift();
