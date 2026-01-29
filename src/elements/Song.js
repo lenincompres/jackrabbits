fe512cd5c38ddecc478f15062f95598ee3ffdd77
Lenin Compres <lenincompres@gmail.com>
2026-01-29 16:07:31 -0500

    update

--- src/elements/Song.js
+++ src/elements/Song.js
@@ -224,7 +224,7 @@ new Song(Copy.text({
 
 new Song(Copy.text({
   es: "https://cdn1.suno.ai/375b6726-de0c-4073-8f67-7d30a6a8f5ad.m4a",
-  en: "https://cdn1.suno.ai/aef0aabc-cea8-4e07-9be6-c54fa1c1f7fd.m4a",
+  en: "https://cdn1.suno.ai/8b707e26-436a-4464-a338-24af4b29eb1e.m4a",
 }), Copy.text({
   es: "Tréboles: a volar entre las torres",
   en: "Clovers: Fly from King Towers",
