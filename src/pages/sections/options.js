95258e8ff4ef831b1da49016052f5af447c40ba0
Lenin Compres <lenincompres@gmail.com>
2026-06-25 11:25:47 -0400

    update

--- src/pages/sections/options.js
+++ src/pages/sections/options.js
@@ -1,29 +1,35 @@
 import Copy from "../../../lib/Copy.js";
 import versify from "../../aux/versify.js";
 
-const optionalSections = [{
-  h4: Copy.text({
-    es: "Finales",
-    en: "Endings",
-  }),
-  section: [{
-    h5: Copy.text({
-      es: "Corte Triple",
-      en: "Triple Court",
+const optionalSections = [
+  {
+    h4: Copy.text({
+      es: "Finales",
+      en: "Endings",
     }),
-    p: versify(Copy.text({
-      es: `Un fin que del <a onclick="popUp('endings', event)"><b>diverso</b></a> se deriva:
+    section: [
+      {
+        h5: Copy.text({
+          es: "Corte Triple",
+          en: "Triple Court",
+        }),
+        p: versify(
+          Copy.text({
+            es: `Un fin que del <a onclick="popUp('endings', event)"><b>diverso</b></a> se deriva:
         tus nobles con la letra repetida.`,
-      en: `Like <a onclick="popUp('endings', event)"><b>diverse</b></a>, but slightly rarer:
+            en: `Like <a onclick="popUp('endings', event)">diverse</a>, but slightly rarer:
         get three nobles with one letter.`,
-    })),
-  }, {
-    h5: Copy.text({
-      es: "Partida corta",
-      en: "Rabbits Hold’em",
-    }),
-    p: versify(Copy.text({
-      es: `La búsqueda de cartas se aligera,
+          }),
+        ),
+      },
+      {
+        h5: Copy.text({
+          es: "Partida corta",
+          en: "Rabbits Hold’em",
+        }),
+        p: versify(
+          Copy.text({
+            es: `La búsqueda de cartas se aligera,
         sin que haya que llevarlas al castillo.
         Si obtienes de las nobles tu tercera,
         descansa por un turno decisivo.
@@ -35,56 +41,171 @@ const optionalSections = [{
         si empatan en el orden recibido;
         se suman los valores que contengan,
         y veinte por un doble es añadido.`,
-      en: `For a quick Jack Rabbits match,
+            en: `For a quick <b>Jack Rabbits</b> match,
         end before the courts arrive.
         Once you hold three royal cards,
         others get one turn to strive.
         Step off-board and simply watch—
         bring the cards you hold to five.
         Rank the courts by rising match:
-        <a onclick="popUp('endings', event)">plain, diverse, monarchic, tribe</a>.
+        from the <a onclick="popUp('endings', event)">simple to the tribe</a>.
         Number cars may boost the ranks,
         if you have to break a tie;
         higher cards mean higher marks,
         but a pair beats single high.`,
-    })),
-  }],
-}, {
-  h4: Copy.text({
-    es: "Ases",
-    en: "Aces",
-  }),
-  section: [{
-    h5: Copy.text({
-      es: "Ases Ambivalentes",
-      en: "Ambivalent Aces",
+          }),
+        ),
+      },
+    ],
+  },
+  {
+    h4: Copy.text({
+      es: "Desafíos",
+      en: "Challenges",
     }),
-    p: versify(Copy.text({
-      es: `Elige los valores de tus ases,
+    section: [
+      {
+        h5: Copy.text({
+          es: `Ciudadela Avanzada`,
+          en: `Advanced Citadel`,
+        }),
+        p: versify(
+          Copy.text({
+            es: `Si eliges la <a class="pop" onclick="popUp('advancedCitadel', event)">carátula de atrás</a>,
+      incluyes estrategias importantes:
+      cuestionas corazones al entrar,
+      canjeas lo que quieras con diamantes,
+      y puedes con tus tréboles volar
+      con palos cualesquiera de restante.`,
+            en: `There are three depicted assets,
+      on this <a class="pop" onclick="popUp('advancedCitadel', event)">flip-side tile</a> reboot:
+      vet the hearts to grant the access;
+      buy with diamonds any loot; 
+      show your clubs for flying passes—
+      shortfall’s paid in any suit.`,
+          }),
+        ),
+      },
+      {
+        h5: Copy.text({
+          es: "Lotería Gorda",
+          en: "Windfall Lottery",
+        }),
+        p: versify(
+          Copy.text({
+            es: `Te premia por aciertos demostrados.
+        Duplica con un doble de los dados.`,
+            en: `Grant all matching cards you hold.
+        Double gains per double rolled.`,
+          }),
+        ),
+      },
+      {
+        h5: Copy.text({
+          es: "Manos Extralimitadas",
+          en: "Overtaxed Hands",
+        }),
+        p: versify(
+          Copy.text({
+            es: `Las cartas se tendrán en escasez.
+        Se cuenta con un máximo de seis.`,
+            en: `Trim the cards you hold on to. 
+        Up to six you may accrue. `,
+          }),
+        ),
+      },
+      {
+        h5: Copy.text({
+          es: "Excedente Expropiado",
+          en: "Seized Surplus",
+        }),
+        p: versify(
+          Copy.text({
+            es: `Cuidado de excederte en lo que tomas.        
+        Quienquiera que lo note te lo roba.`,
+            en: `Don't get caught with cards abound.
+        Your revealer robs you down.`,
+          }),
+        ),
+      },
+      /*}, {,{
+      h5__: Copy.text({
+        es: "Tablero Urbano",
+        en: "Urban Board",
+      }),
+      p__: versify(Copy.text({
+        es: `Conecta los hexágonos directo,
+        y olvídate <a class="pop" onclick="popUp('roadTiles')">de rombos y de triángulos</a>;
+        la falta de traslado en los trayectos
+        hará que se incrementen los obstáculos.`,
+        en: `To play this version correctly,
+        make a land of shorter miles,
+        connecting hexes directly
+        and removing all <a class="pop" onclick="popUp('roadTiles')">road tiles</a>.`,
+      }))
+      },*/
+    ],
+  },
+  {
+    h4: Copy.text({
+      es: "Cartas",
+      en: "Cards",
+    }),
+    section: [
+      {
+        h5: Copy.text({
+          es: "Ases Ambivalentes",
+          en: "Ambivalent Aces",
+        }),
+        p: versify(
+          Copy.text({
+            es: `Elige los valores de tus ases,
         que cuentan por un <b>1</b> o por un <b>11</b>,
         ya sea en el momento en que los gastes
         o incluso en el momento que los compres.`,
-      en: `Count their value as <b>11</b>,
+            en: `Count their value as <b>11</b>,
       any time you play an <b>A</b>ce.
       Or a <b>1</b> when it's deserving—
       lottery or marketplace.`,
-    })),
-  }, {
-    h5: Copy.text({
-      es: "Ases capitales",
-      en: "Capital Aces", 
-    }),
-    p: versify(Copy.text({
-      es: `El as que de primero desembolses,
+          }),
+        ),
+      },
+      {
+        h5: Copy.text({
+          es: "Ases Capitales",
+          en: "Capital Aces",
+        }),
+        p: versify(
+          Copy.text({
+            es: `El as que de primero desembolses,
         descártalo y consérvalo en tu frente;
         pues éste se valora por un <b>11</b>
         y solo por un <b>1</b> tus siguientes.`,
-      en: `Your first <b>A</b>ce is worth <b>11</b>,
+            en: `Your first <b>A</b>ce is worth <b>11</b>,
         and the rest you'll use are <b>1</b>s.
         As a record that it’s spent then, 
         keep it laid before your hands.`,
-    })),
-  }, {
+          }),
+        ),
+      },
+      {
+        h5: Copy.text({
+          es: "Valores Patriarcales",
+          en: "Patriarchal values",
+        }),
+        p: versify(
+          Copy.text({
+            es: `Valoras a los ases por <b>14</b>;
+        los reyes, es por <b>13</b> que valoras;
+        las reinas, subvalóralas por <b>12</b>;
+        y es <b>11</b> (menos precio) por las jotas.`,
+            en: `Have unequal value levels:
+        <b>A</b>ce then <b>K</b>ing then <b>Q</b>ueen then <b>J</b>ack—
+        from <b>14</b> until <b>11</b>—
+        contemplate why this is whack.`,
+          }),
+        ),
+      } /*{
     h5: Copy.text({
       es: "Capitales adicionales",
       en: "Additional capital",
@@ -99,106 +220,73 @@ const optionalSections = [{
         At the suit-appropriate places,
         they will grant an extra pip.`,
     })),
-  }],
-}, {
-  h4: Copy.text({
-    es: "Piezas",
-    en: "Tiles",
-  }),
-  section: [{
-    h5: Copy.text({
-      es: `Ciudadela Avanzada`,
-      en: `Advanced Citadel`,
+  }*/,
+    ],
+  },
+  {
+    h4: Copy.text({
+      es: "Palos",
+      en: "Pips",
     }),
-    p: versify(Copy.text({
-      es: `Si eliges la <a class="pop" onclick="popUp('advancedCitadel', event)">carátula de atrás</a>,
-      incluyes estrategias importantes:
-      cuestionas corazones al entrar,
-      canjeas lo que quieras con diamantes,
-      y puedes con tus tréboles volar
-      con palos cualesquiera de restante.`,
-      en: `There are three depicted assets,
-      on this <a class="pop" onclick="popUp('advancedCitadel', event)">flip-side tile</a> reboot:
-      vet the hearts to grant the access;
-      buy with diamonds any loot; 
-      show your clubs for flying passes—
-      shortfall’s paid in any suit.`,
-    })),
-  }, {
-    h5: Copy.text({
-      es: "Palos Justos",
-      en: "Tight Suits",
-    }),
-    p: versify(Copy.text({
-      es: `Se juega con un palo por conejo
+    section: [
+      {
+        h5_spades: Copy.text({
+          es: "Picas Potenciadas",
+          en: "Spades Boost",
+        }),
+        p: versify(
+          Copy.text({
+            es: `Se juega con un palo por conejo
       y un <a class="pop" onclick="popUp('roadTiles', event)">set de carreteras</a> a su vez.
       Con tres participantes, por ejemplo,
       retírales un palo con un set.
       Contempla al escoger el <a class="pop" onclick="popUp('endings', event)">fin del juego</a>
-      si acaso es imposible de obtener.
-      <b class="spades">Potencias a las picas</b> y sus duelos
-      con cartas de nobleza en escasez.`,
-      en: `Have as many suits as players,
-      and a set of <a class="pop" onclick="popUp('roadTiles', event)">road tiles</a> too. 
+      si acaso es imposible de obtener.`,
+            en: `Have as many suits as players—
+royals scarce and roads reduced. 
       Say there's only three contenders, 
-      then remove a set and suit.
+      then <a class="pop" onclick="popUp('roadTiles', event)">remove a set</a> and suit.
       Pick a fitting kind of ending:
-      one that cards allow you to.
-      Even if it’s unintended,
-      playing tight gives <b class="spades">spades</b> a <b class="spades">boost</b>.`,
-    })),
-    /*},{
-      h5__: Copy.text({
-        es: "Tablero Urbano",
-        en: "Urban Board",
-      }),
-      p__: versify(Copy.text({
-        es: `Conecta los hexágonos directo,
-        y olvídate <a class="pop" onclick="popUp('roadTiles')">de rombos y de triángulos</a>;
-        la falta de traslado en los trayectos
-        hará que se incrementen los obstáculos.`,
-        en: `To play this version correctly,
-        make a land of shorter miles,
-        connecting hexes directly
-        and removing all <a class="pop" onclick="popUp('roadTiles')">road tiles</a>.`,
-      })),*/
-  }],
-}, {
-  h4: Copy.text({
-    es: "Estrategias",
-    en: "Strategies",
-  }),
-  section: [{
-    h5_clubs: Copy.text({
-      es: "Tréboles Potenciados",
-      en: "Clovers Boost",
-    }),
-    p: versify(Copy.text({
-      es: `Las plazas, Ciudadela y los palacios
+      one that cards allow you to.`,
+          }),
+        ),
+      },
+      {
+        h5_clubs: Copy.text({
+          es: "Tréboles Potenciados",
+          en: "Clovers Boost",
+        }),
+        p: versify(
+          Copy.text({
+            es: `Las plazas, Ciudadela y los palacios
         se añaden al destino de los vuelos.
         Podrás aterrizar en los hexágonos,
         excepto por supuesto en el subsuelo.`,
-      en: `When you fly from any tower, 
+            en: `When you fly from any tower, 
         find more places to touch down.
         Clubs expand their landing power—
         any hex above the ground.`,
-    })),
-  }, {
-    h5_hearts: Copy.text({
-      es: "Corazones Potenciados",
-      en: "Hearts Boost",
-    }),
-    p: versify(Copy.text({
-      es: `Si llegas a un lugar con zanahoria,
+          }),
+        ),
+      },
+      {
+        h5_hearts: Copy.text({
+          es: "Corazones Potenciados",
+          en: "Hearts Boost",
+        }),
+        p: versify(
+          Copy.text({
+            es: `Si llegas a un lugar con zanahoria,
       te pueden cuestionar por credenciales;
       pedir validación satisfactoria,
       tal como en los palacios señoriales.`,
-      en: `Any hex a carrot marks 
+            en: `Any hex a carrot marks 
       is considered palace grounds.
       Anyone may vet with hearts 
       as a player comes around.`,
-    })),
-  }, {
+          }),
+        ),
+      } /*{
     h5: Copy.text({
       es: "Zanahorias ventajosas",
       en: "Trailing Carrots",
@@ -213,61 +301,9 @@ const optionalSections = [{
         Royals claimed provide a trace—
         you may leave in the same round.`,
     })),
-  }]
-}, {
-  h4: Copy.text({
-    es: "Cartas",
-    en: "Cards",
-  }),
-  section: [{
-    h5: Copy.text({
-      es: "Lotería Gorda",
-      en: "Windfall Lottery",
-    }),
-    p: versify(Copy.text({
-      es: `Te premia por aciertos demostrados.
-        Duplica con un doble de los dados.`,
-      en: `Grant all matching cards you hold.
-        Double gains per double rolled.`,
-    })),
-  }, {
-    h5: Copy.text({
-      es: "Manos Extralimitadas",
-      en: "Overtaxed Hands",
-    }),
-    p: versify(Copy.text({
-      es: `Las cartas se tendrán en escasez.
-        Se cuenta con un máximo de seis.`,
-      en: `Trim the cards you hold on to. 
-        Up to six you may accrue. `,
-    })),
-  }, {
-    h5: Copy.text({
-      es: "Excedente Expropiado",
-      en: "Seized Surplus",
-    }),
-    p: versify(Copy.text({
-      es: `Cuidado de excederte en lo que tomas.        
-        Quienquiera que lo note te lo roba.`,
-      en: `Don't get caught with cards abound.
-        Your revealer robs you down.`,
-    })),
-  }, {
-    h5: Copy.text({
-      es: "Valores Patriarcales",
-      en: "Patriarchal values",
-    }),
-    p: versify(Copy.text({
-      es: `Valoras a los ases por <b>14</b>;
-        los reyes, es por <b>13</b> que valoras;
-        las reinas, subvalóralas por <b>12</b>;
-        y es <b>11</b> (menos precio) por las jotas.`,
-      en: `Have unequal value levels:
-        <b>A</b>ce then <b>K</b>ing then <b>Q</b>ueen then <b>J</b>ack—
-        from <b>14</b> until <b>11</b>—
-        contemplate why this is whack.`,
-    })),
-  }]
-}];
+  },*/,
+    ],
+  },
+];
 
 export default optionalSections;
